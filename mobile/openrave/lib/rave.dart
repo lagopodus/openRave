3f81738e6c565419c3cfba1da3ce15b953c9b8b9
Alexander Betke <main@alexinabox.de>
2025-01-29 00:53:21 +0100

    implement room code validation and create Rave page

--- mobile/openrave/lib/rave.dart
+++ mobile/openrave/lib/rave.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'rave.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class Rave extends StatefulWidget {
+  const Rave({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  State<Rave> createState() => _RaveState();
 }
 
-class _HomeState extends State<Home> {
-  String _roomCode = '';
+class _RaveState extends State<Rave> {
+  String _roomCode = '12334';
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        middle: Text('OpenRave'),
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('OpenRave in Room $_roomCode'),
       ),
       child: Center(
         child: Column(
@@ -56,34 +55,7 @@ class _HomeState extends State<Home> {
                 const SizedBox(width: 20),
                 CupertinoButton.filled(
                   borderRadius: BorderRadius.circular(7),
-                  onPressed: () {
-                    //If the room code is of valid lenght (6) and has digits only, then proceed to join the rave.
-                    if (_roomCode.length == 6 && isNumeric(_roomCode)) {
-                      Navigator.of(context).push(
-                        CupertinoPageRoute<void>(
-                          builder: (context) => const Rave(),
-                        ),
-                      );
-                      return;
-                    } else {
-                      showCupertinoModalPopup(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return CupertinoAlertDialog(
-                            title: Text('Invalid Room Code'),
-                            content: Text(
-                                'The room code you entered is not valid. Please try again.'),
-                            actions: [
-                              CupertinoDialogAction(
-                                child: Text('OK'),
-                                onPressed: () => Navigator.of(context).pop(),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    }
-                  },
+                  onPressed: () {},
                   child: const Text('Join a Rave!'),
                 ),
               ],
@@ -93,11 +65,4 @@ class _HomeState extends State<Home> {
       ),
     );
   }
-}
-
-bool isNumeric(String s) {
-  if (s == null) {
-    return false;
-  }
-  return double.tryParse(s) != null;
 }
